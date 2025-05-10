bebf3775a93120dc72b2f89c307ab00e7f4e35af
KP0823 <kushparmar0823@gmail.com>
2025-05-10 09:31:56 -0400

    I am an idiot

--- sql_queries/q3_sql.sql
+++ sql_queries/q3_sql.sql
@@ -1,19 +1,4 @@
-with t1 as (
-	select prod, month, min(quant) min_quant
-	from sales
-	group by prod, month
-),
-t2 as (
-	select prod, month, max(quant) max_quant
-	from sales
-	group by prod, month
-),
-t3 as (
-	select prod, month, avg(quant) avg_quant
-	from sales
-	group by prod, month
-)
-select t1.prod, t1.month, min_quant, max_quant, avg_quant
-from t1, t2, t3
-where t1.prod = t2.prod and t1.prod = t3.prod and t1.month = t2.month and t1.month = t3.month
-order by t1.prod, t1.month
+select prod, month, min(quant), max(quant), avg(quant)
+from sales
+group by prod, month
+order by prod, month
